5a36583d0773c10181614817bdd394ad6f57b021
MUTWIRI0922 <amutwiri07@gmail.com>
2025-03-17 17:08:12 +0300

    checkout

---
+++ checkout.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Checkout</title>
+</head>
+<body>
+    <header>
+        <div class="logo-container">
+            <img src="Papalucas/logo.svg" alt="Papalucas Stores Logo" class="logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="papalucas.html">Home</a></li>
+                <li><a href="products.html">Shop</a></li>
+                <li><a href="cart.html">Cart</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+        <?php
+
+        ?>
+        <table>
+            <tr>
+            <th>Item</th>
+            <th>Unit Price</th>
+            <th>Quantity</th>
+            <th>Total Price</th>
+            </tr>
+            <tr>
+                foreach($_SESSION['cart'] as $item){
+                <td><?php echo $item['name']; ?></td>
+                <td></td>
+            </tr>
+
+    
+        </table>
+</body>
+</html>
+
