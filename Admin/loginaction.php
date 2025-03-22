02d400bed33683a2bf7b9a51c845ab225324eb7f
MUTWIRI0922 <amutwiri07@gmail.com>
2025-03-22 08:22:24 +0300

    validation

--- Admin/loginaction.php
+++ Admin/loginaction.php
@@ -2,10 +2,18 @@
 $Hostname = 'localhost';
 $user = '';
 $dbName= 'papa lucas';
-$conn = new mysqli($localhost, $user, $Password, $dbName, '');
+$conn = new PDO($Hostname, $Password, $dbName);
+
 // capture form inputs
     $Username = $_POST['Username'];
     $Password = $_POST['Password'];
+    
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
 // check if form is submitted
 if(isset($_POST['submit'])){
     if($Username == "admin" && $Password == "admin"){
